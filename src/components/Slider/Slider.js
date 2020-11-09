1ef651f430e3dc324197b5156805c28ef878baa0
FBIV7990 <developers@mac9.in>
2020-11-09 17:33:15 +0530

    clean

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -1,32 +1,3 @@
-// import React from "react";
-// import "../Style.css";
-// import AliceCarousel from "react-alice-carousel";
-// import "react-alice-carousel/lib/alice-carousel.css";
-// import image1 from "../../img/background check.jpg";
-// import image2 from "../../img/background verification.jpg";
-// import image3 from "../../img/contractor license.jpg";
-// import image4 from "../../img/tenant verification.jpg";
-// import image5 from "../../img/vehicle verification.jpg";
-
-// export default function Slider() {
-//   return (
-//     <div className="App">
-//       <AliceCarousel
-//         dotsDisabled={false}
-//         autoPlay={true}
-//         duration={4000}
-//         alice-carousel__prev-btn-wrapper={false}
-//         mouseDragEnabled={false}
-//       >
-//         <img src={image1} className="sliderimg" alt="" />
-//         <img src={image2} className="sliderimg" alt="" />
-//         <img src={image3} className="sliderimg" alt="" />
-//         <img src={image4} className="sliderimg" alt="" />
-//         <img src={image5} className="sliderimg" alt="" />
-//       </AliceCarousel>
-//     </div>
-//   );
-// }
 import React from "react";
 import slider1 from "../../img/background check.jpg";
 import slider2 from "../../img/background verification.jpg";
@@ -48,7 +19,6 @@ class Slider extends React.Component {
         autoPlay={3000}
         infinite={true}
         offset={0}
-        // itemWidth={1400}
         clickToChange
         centered
       >
